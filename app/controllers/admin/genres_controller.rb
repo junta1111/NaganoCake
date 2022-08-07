fe027564b16b8d73e0d5d9f3795443cee0ce4586
EC2 Default User <ec2-user@ip-172-31-77-85.ec2.internal>
2022-08-07 13:09:02 +0000

    naganocake_2

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,2 +1,27 @@
 class Admin::GenresController < ApplicationController
+  def index
+    @genres = Genre.all
+    @genre = Genre.new
+  end
+  
+  def create
+    genre = Genre.new
+    genre.save
+    redirect_to 'index'
+  end
+  
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+  
+  def update
+    @genre = Genre.find(params[:id])
+    genre.update(genre_params)
+    redirect_to genre_path(genre.id)
+  end
+  
+  private
+  def genre_params
+    params.require(:genre).permit(:genre)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # URL /customers/sign_in ...
-devise_for :customers,skip: [:passwords], controllers: {
+ devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
@@ -10,5 +10,14 @@ devise_for :customers,skip: [:passwords], controllers: {
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
+get 'genres' => 'admin/genres#index'
+get 'genres/:id/edit' => 'admin/genres#edit', as: 'edit_genre'
+patch 'genres/:id' => 'admin/genres#update', as: 'update_genre'
+post 'admin/genres' => 'admin/genres#create', as: 'create_genre'
+
+get 'items' => 'admin/items#index'
+get 'items/new'
+get 'admin/items/new' => 'admin/items#new'
+post 'admin/items' => 'admin/items#create', as: 'create_item'
 
 end
